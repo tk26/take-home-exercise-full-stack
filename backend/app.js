64f017506c048642f53a38d698246344af2ef0c8
Tejas Kumthekar <tejas.kumthekar26@gmail.com>
2020-09-04 07:48:47 -0500

    Add POST team endpoint with more data layer validations

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { ValidationError } = require('sequelize');
+
 const { TeamMember } = require('./model');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/team', async (req, res, next) => {
   const team = await TeamMember.findAll();
   return res.json(team);
+});
+
+app.post('/team', async (req, res, next) => {
+  try {
+    const member = await TeamMember.create(req.body);
+    console.log(`Member created successfully: ${JSON.stringify(member)}`);
+    return res.sendStatus(201);
+  } catch (error) {
+    console.error(`An error occurred: ${JSON.stringify(error)}`);
+    if (error instanceof ValidationError) {
+      return res.status(400).send(error.errors[0].message);
+    }
+    return res.sendStatus(500);
+  }
 });
 
 module.exports = app;
--- backend/model.js
+++ backend/model.js
@@ -11,25 +11,73 @@ TeamMember.init(
     // attributes
     firstName: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter your first name'
+        },
+        len: {
+          args: [1, 255],
+          msg: "First name should be 1 to 255 characters long"
+        }
+      }
     },
     lastName: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter your last name'
+        },
+        len: {
+          args: [1, 255],
+          msg: "Last name should be 1 to 255 characters long"
+        }
+      }
     },
     title: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter your title'
+        },
+        len: {
+          args: [1, 255],
+          msg: "Title should be 1 to 255 characters long"
+        }
+      }
     },
     story: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please tell us your story'
+        },
+        len: {
+          args: [1, 500],
+          msg: "Story should be 1 to 500 characters long"
+        }
+      }
     },
     favoriteColor: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      validate: {
+        is: {
+          args: ["^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",'i'],
+          msg: "Please enter your favorite color in hexadecimal"
+        }
+      }
     },
     photoUrl: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      validate: {
+        isUrl: {
+          args: true,
+          msg: "Please enter a valid photo URL"
+        }
+      }
     }
   },
   {
